8d134a94b83da48a3e403a229c05347d8827e78a
mountain greatest <mountain.itstl@gmail.com>
2022-08-10 22:27:20 +0700

    second commit

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web.js";
+import connectDB from "./config/connectDB";
 require('dotenv').config();
 
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 viewEngine(app);
 initWebRoutes(app);
+connectDB();
 
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
